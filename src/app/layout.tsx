6a74f155f0cbebca453e4e123d559e2ffa0dfcc3
Ali <Razzaq.Ali@bakerhughes.com>
2023-07-20 20:07:37 +0530

    outline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,7 @@
+import Notification from '@/components/Notification'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Notification />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }

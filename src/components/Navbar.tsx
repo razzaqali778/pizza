b4f001cf284f204d1d321edef1cc81d0cd45a6be
Ali <Razzaq.Ali@bakerhughes.com>
2023-07-20 21:31:30 +0530

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Menu from './Menu'
+import Link from 'next/link'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className="h-12 text-red-500 p-4 flex items-center justify-center border-b-2 border-b-red-500 uppercase">
+      <div className="text-xl">
+        <Link href="/">Massimo</Link>
+      </div>
+      <Menu />
+    </div>
+  )
 }
 
 export default Navbar
